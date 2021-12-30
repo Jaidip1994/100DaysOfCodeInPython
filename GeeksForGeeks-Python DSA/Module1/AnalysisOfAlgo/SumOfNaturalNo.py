f060dd3f02945535a4c2637bd510a5a00f5ade00
Jaidip Ghosh <jaidip1994@gmail.com>
2021-12-30 13:21:41 +0530

    Asymptotic Analysis

--- GeeksForGeeks-Python DSA/Module1/AnalysisOfAlgo/SumOfNaturalNo.py
+++ GeeksForGeeks-Python DSA/Module1/AnalysisOfAlgo/SumOfNaturalNo.py
@@ -7,7 +7,20 @@
 # o/p = 15
 
 i = int(input('Enter the number: '))
-sum_op = 0
-for i in range(1, i+1):
-    sum_op += i
-print(f'Sum of n natural numbers is : {sum_op}')
+
+def sol1(i):
+    sum_op = 0
+    for i in range(1, i+1):
+        sum_op += i
+    return sum_op
+
+
+sum_op = sol1(i)
+print(f'Sol 1 Sum of n natural numbers is : {sum_op}')
+
+
+def sol2(i):
+    return int((i*(i+1))/2)
+
+sum_op = sol2(i)
+print(f'Sol 2 Sum of n natural numbers is : {sum_op}')
---
+++ GeeksForGeeks-Python DSA/readme.md
@@ -0,0 +1,6 @@
+# Points Learnt from Course
+- What is Asymptotic Analysis
+
+    1. No dependency on the Machine, programming languages , etc
+    2. No need to implement all the ideas/algos, even the pseudo code works
+    3. Asymptotic Analysis is about measuring order of growth in terms of input size. 
